d0160588c7c7d2304fbecae00eeaa44907eebb56
Jessica Mosby <jessicamosby@mac.com>
2022-11-17 18:26:36 -0500

    changed all table elements to h3 so titles could be h1

--- render-utils.js
+++ render-utils.js
@@ -1,8 +1,8 @@
 export function renderCars(cars) {
     const div = document.createElement('div');
 
-    const h2 = document.createElement('h2');
-    h2.textContent = cars.model;
+    const h3 = document.createElement('h3');
+    h3.textContent = cars.model;
 
     const p = document.createElement('p');
     p.textContent = `This is a ${cars.model} made by ${cars.make} in ${cars.year}.`;
@@ -13,45 +13,45 @@ export function renderCars(cars) {
     //     li.textContent = car;
     //     ul.append(li);
     // }
-    div.append(h2, p);
+    div.append(h3, p);
     return div;
 }
 
 export function renderMonies(money) {
     const div = document.createElement('div');
 
-    const h2 = document.createElement('h2');
-    h2.textContent = money.currency;
+    const h3 = document.createElement('h3');
+    h3.textContent = money.currency;
 
     const p = document.createElement('p');
     p.textContent = `In ${money.country} the currency is ${money.currency} and the currency code is ${money.currency_code}.`;
 
-    div.append(h2, p);
+    div.append(h3, p);
     return div;
 }
 
 export function renderNames(names) {
     const div = document.createElement('div');
 
-    const h2 = document.createElement('h2');
-    h2.textContent = names.full_name;
+    const h3 = document.createElement('h3');
+    h3.textContent = names.full_name;
 
     const p = document.createElement('p');
     p.textContent = `Full name: ${names.full_name} American: ${names.american} European: ${names.european}`;
 
-    div.append(h2, p);
+    div.append(h3, p);
     return div;
 }
 
 export function renderStreets(streets) {
     const div = document.createElement('div');
 
-    const h2 = document.createElement('h2');
-    h2.textContent = streets.name;
+    const h3 = document.createElement('h3');
+    h3.textContent = streets.name;
 
     const p = document.createElement('p');
     p.textContent = `You live on ${streets.number} ${streets.name} ${streets.suffix}`;
 
-    div.append(h2, p);
+    div.append(h3, p);
     return div;
 }
